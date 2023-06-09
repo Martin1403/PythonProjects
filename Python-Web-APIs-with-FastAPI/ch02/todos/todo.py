403d4c3671f066685584aa57307120f939cf92ab
Martin <martinvajkuny@gmai.com>
2023-06-09 17:17:42 +0200

    update

--- Python-Web-APIs-with-FastAPI/ch02/todos/todo.py
+++ Python-Web-APIs-with-FastAPI/ch02/todos/todo.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from fastapi import APIRouter
 
 todo_router = APIRouter()
@@ -7,3 +8,9 @@ todo_list = []
 @todo_router.post("/todo")
 async def add_todo(todo: dict) -> dict:
     todo_list.append(todo)
+    return {"message": "Todo added successfully"}
+
+
+@todo_router.get("/todo")
+async def retrieve_todos() -> dict:
+    return {"todos": todo_list}
