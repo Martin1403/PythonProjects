c4bb25c037c0449d91250b8baa0f0a1a9bf46677
Martin <martinvajkuny@gmai.com>
2023-06-20 17:02:52 +0200

    update

--- Udemy - Python 3 Deep Dive (Part 4 - OOP) 2022-4/notebooks/07 - Project 3/inventory/app/models/inventory.py
+++ Udemy - Python 3 Deep Dive (Part 4 - OOP) 2022-4/notebooks/07 - Project 3/inventory/app/models/inventory.py
@@ -150,4 +150,179 @@ class CPU(Resource):
     """Resource subclass used to track specific CPU inventory pools"""
 
     def __init__(self, name, manufacturer, total, allocated, cores, socket, power_watts):
-        
+        """
+        Args:
+            name (str): display name of resource
+            manufacturer (str): resource manufacturer
+            total (int): current total amount of resources
+            allocated (int): current count of in-use resources
+            cores (int): number of cores
+            socket (str): CPU socket type
+            power_watts (int): CPU rated wattage
+        """
+        super().__init__(name, manufacturer, total, allocated)
+
+        validate_integer("cores", cores, 1)
+        validate_integer("power_watts", power_watts, 1)
+
+        self._cores = cores
+        self._socket = socket
+        self._power_watts = power_watts
+
+    @property
+    def cores(self):
+        """
+        Number of cores.
+
+        Returns:
+            int
+        """
+        return self._cores
+
+    @property
+    def socket(self):
+        """
+        The socket type for this CPU
+        Returns:
+            str
+        """
+        return self._socket
+
+    @property
+    def power_watts(self):
+        """
+        The rated wattage of this CPU
+
+        Returns:
+            int
+        """
+        return self._power_watts
+
+    def __repr__(self):
+        return f"{self.category}: {self.name} ({self.socket} - x{self.cores})"
+
+
+class Storage(Resource):
+    """
+    A base class for storage devices - probably not used directly
+    """
+
+    def __init__(self, name, manufacturer, total, allocated, capacity_gb):
+        """
+
+        Args:
+            name (str): display name of resource
+            manufacturer (str): resource manufacturer
+            total (int): current total amount of resources
+            allocated (int): current count of in-use resources
+            capacity_gb (int): storage capacity (in GB)
+        """
+
+        super().__init__(name, manufacturer, total, allocated)
+        validate_integer('capacity_gb', capacity_gb, 1)
+        self._capacity_gb = capacity_gb
+
+    @property
+    def capacity_gb(self):
+        """
+        Indicates the capacity (in GB) of the storage device
+
+        Returns:
+            int
+        """
+        return self._capacity_gb
+
+    def __repr__(self):
+        return f'{self.category}: {self.capacity_gb} GB'
+
+
+class HDD(Storage):
+    """
+    Class used for HDD type resources
+    """
+    def __init__(
+            self, name, manufacturer, total, allocated, capacity_gb,
+            size, rpm
+    ):
+        """
+
+        Args:
+            name (str): display name of resource
+            manufacturer (str): resource manufacturer
+            total (int): current total amount of resources
+            allocated (int): current count of in-use resources
+            capacity_gb (int): storage capacity (in GB)
+            size (str): indicates the device size (must be either 2.5" or 3.5")
+            rpm (int): disk rotation speed (in rpm)
+        """
+        super().__init__(name, manufacturer, total, allocated, capacity_gb)
+
+        allowed_sizes = ['2.5"', '3.5"']
+        if size not in allowed_sizes:
+            raise ValueError(f'Invalid HDD size. '
+                             f'Must be one of {", ".join(allowed_sizes)}')
+        validate_integer('rpm', rpm, min_value=1_000, max_value=50_000)
+
+        self._size = size
+        self._rpm = rpm
+
+    @property
+    def size(self):
+        """
+        The HDD size (2.5" / 3.5")
+
+        Returns:
+            str
+        """
+        return self._size
+
+    @property
+    def rpm(self):
+        """
+        The HDD spin speed (rpm)
+
+        Returns:
+            int
+        """
+        return self._rpm
+
+    def __repr__(self):
+        s = super().__repr__()
+        return f'{s} ({self.size}, {self.rpm} rpm)'
+
+
+class SSD(Storage):
+    """
+    Class used for SSD type resources
+    """
+    def __init__(
+            self, name, manufacturer, total, allocated, capacity_gb,
+            interface
+    ):
+        """
+
+        Args:
+            name (str): display name of resource
+            manufacturer (str): resource manufacturer
+            total (int): current total amount of resources
+            allocated (int): current count of in-use resources
+            capacity_gb (int): storage capacity (in GB)
+            interface (str): indicates the device interface (e.g. PCIe NVMe 3.0 x4)
+        """
+        super().__init__(name, manufacturer, total, allocated, capacity_gb)
+
+        self._interface = interface
+
+    @property
+    def interface(self):
+        """
+        Interface used by SSD (e.g. PCIe NVMe 3.0 x4)
+
+        Returns:
+            str
+        """
+        return self._interface
+
+    def __repr__(self):
+        s = super().__repr__()
+        return f'{s} ({self.interface})'
